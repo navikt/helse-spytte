f8e7fce387a8b5caef16c06b163fd3c913499a7d
David Steinsland <david@steinsland.email>
2022-02-05 16:08:14 +0100

    bygger en kjørbar jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,37 @@ repositories {
 val rapidsAndRiversCliVersion = "1.be82170"
 
 dependencies {
-    implementation(kotlin("stdlib"))
     implementation("com.github.navikt:rapids-and-rivers-cli:$rapidsAndRiversCliVersion")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    named<Jar>("jar") {
+        archiveFileName.set("app.jar")
+
+        manifest {
+            attributes["Main-Class"] = "no.nav.helse.spytte.AppKt"
+            attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+                it.name
+            }
+        }
+
+        doLast {
+            configurations.runtimeClasspath.get().forEach {
+                val file = File("$buildDir/libs/${it.name}")
+                if (!file.exists())
+                    it.copyTo(file)
+            }
+        }
+    }
+
+    withType<Wrapper> {
+        gradleVersion = "7.3.3"
+    }
 }
