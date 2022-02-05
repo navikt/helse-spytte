c5ac3be62186a1c800c72b96514059793abfc764
David Steinsland <david@steinsland.email>
2022-02-05 16:18:40 +0100

    eksempel på hvordan sende én melding

--- src/main/kotlin/no/nav/helse/spytte/App.kt
+++ src/main/kotlin/no/nav/helse/spytte/App.kt
@@ -1,5 +1,9 @@
 package no.nav.helse.spytte
 
+import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.rapids_and_rivers.cli.AivenConfig
 import no.nav.rapids_and_rivers.cli.ConsumerProducerFactory
 import no.nav.rapids_and_rivers.cli.RapidsCliApplication
@@ -21,6 +25,16 @@ private fun setOffset(factory: ConsumerProducerFactory, consumerGroup: String, t
         }
 }
 
+private fun produserEnkeltmelding(factory: ConsumerProducerFactory, topic: String, key: String, message: String) {
+    factory.createProducer().apply {
+        send(ProducerRecord(topic, key, message)).get()
+    }.close()
+}
+
+private val mapper = jacksonObjectMapper()
+    .registerModule(JavaTimeModule())
+    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+
 fun main() {
     val fnr = "FNR" // TODO les fra liste
 
@@ -29,6 +43,12 @@ fun main() {
     /**
      * Sette offsets basert på tid, f.eks. til 1. desember 2021:
      * setOffset(factory, "min-consumer-gruppe", "tbd.rapid.v1", LocalDateTime.of(2021, 12, 1, 0, 0, 0))
+     */
+
+    /**
+     * Sende en enkeltmelding:
+     * val message = mapper.writeValueAsString(obj)
+     * produserEnkeltmelding(factory, "tbd.rapid.v1", "fnr", message)
      */
 
     @Language("json") val json = """
