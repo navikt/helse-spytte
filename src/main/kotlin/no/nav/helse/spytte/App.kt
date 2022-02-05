7ee12c274cffd17f3805c153388daf9b4fece072
David Steinsland <david@steinsland.email>
2022-02-05 16:46:15 +0100

    eksempel på hvordan konsumere meldinger

--- src/main/kotlin/no/nav/helse/spytte/App.kt
+++ src/main/kotlin/no/nav/helse/spytte/App.kt
@@ -4,18 +4,20 @@ import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import no.nav.rapids_and_rivers.cli.AivenConfig
-import no.nav.rapids_and_rivers.cli.ConsumerProducerFactory
-import no.nav.rapids_and_rivers.cli.RapidsCliApplication
-import no.nav.rapids_and_rivers.cli.seekTo
+import no.nav.rapids_and_rivers.cli.*
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.intellij.lang.annotations.Language
 import org.slf4j.LoggerFactory
 import java.time.LocalDateTime
+import java.util.*
 import kotlin.system.exitProcess
 
 private val log = LoggerFactory.getLogger("no.nav.helse.spytte.App")
 
+/**
+ * Sette offsets basert på tid, f.eks. til 1. desember 2021:
+ * setOffset(factory, "min-consumer-gruppe", "tbd.rapid.v1", LocalDateTime.of(2021, 12, 1, 0, 0, 0))
+ */
 private fun setOffset(factory: ConsumerProducerFactory, consumerGroup: String, topic: String, time: LocalDateTime) {
     RapidsCliApplication(factory)
         .start(consumerGroup, listOf(topic)) { consumer ->
@@ -25,10 +27,57 @@ private fun setOffset(factory: ConsumerProducerFactory, consumerGroup: String, t
         }
 }
 
+/**
+ * Sende en enkeltmelding:
+ * val message = mapper.writeValueAsString(obj)
+ * produserEnkeltmelding(factory, "tbd.rapid.v1", "fnr", message)
+ */
 private fun produserEnkeltmelding(factory: ConsumerProducerFactory, topic: String, key: String, message: String) {
     factory.createProducer().apply {
         send(ProducerRecord(topic, key, message)).get()
     }.close()
+}
+
+/**
+ * Konsumere meldinger:
+ * konsumereMeldinger(factory, "min-consumergruppe", "tbd.rapid.v1")
+ */
+private fun konsumereMeldinger(factory: ConsumerProducerFactory, consumerGroup: String, topic: String) {
+    // kan definere egne consumer properties som overskriver defaults.
+    // For å se hva som er defaults, sjekk Config-implementasjonen som
+    // ConsumerProducerFactory er satt opp med, f.eks.
+    // @see no.nav.rapids_and_rivers.cli.AivenConfig#consumerConfig
+    val consumerProperties = Properties()
+    RapidsCliApplication(factory).apply {
+        JsonRiver(this).apply {
+            // prerequisite er en spesiell type validation som kaster exception dersom den feiler.
+            // det betyr i praksis at meldingen blir ignorert, og riveren fortsetter med neste melding.
+            // onError() blir _ikke_ kalt
+            // prerequisite er det samme som demandKey, demandValue osv. i Rapids and rivers
+            prerequisite { record, node, _ ->
+                true // godta alt
+            }
+            // kan filtrere meldinger basert på info i ConsumerRecord eller JsonNode.
+            // kan legge til årsaker til hvorfor man ikke validerte i $reasons (ikke påkrevd).
+            // validate er det samme som requireKey, requireValue osv. i Rapids and rivers
+            validate { record, node, reasons ->
+                node.path("@event_name").asText() == "vedtaksperiode_endret" // bare lytte på spesielle events
+            }
+            onError { record, node, reasons ->
+                // blir kalt dersom validering feiler
+                println("Validerte ikke melding fordi:")
+                println(reasons.joinToString(separator = "\n"))
+                println("Melding:\n$node")
+            }
+            // onMessage handlers blir kalt i den rekkefølgen man legger dem til
+            onMessage { record, node ->
+                println("leser melding: $node")
+            }
+            onMessage { record, node ->
+
+            }
+        }
+    }.start(consumerGroup, listOf(topic), consumerProperties)
 }
 
 private val mapper = jacksonObjectMapper()
@@ -39,17 +88,6 @@ fun main() {
     val fnr = "FNR" // TODO les fra liste
 
     val factory = ConsumerProducerFactory(AivenConfig.default)
-
-    /**
-     * Sette offsets basert på tid, f.eks. til 1. desember 2021:
-     * setOffset(factory, "min-consumer-gruppe", "tbd.rapid.v1", LocalDateTime.of(2021, 12, 1, 0, 0, 0))
-     */
-
-    /**
-     * Sende en enkeltmelding:
-     * val message = mapper.writeValueAsString(obj)
-     * produserEnkeltmelding(factory, "tbd.rapid.v1", "fnr", message)
-     */
 
     @Language("json") val json = """
         {
